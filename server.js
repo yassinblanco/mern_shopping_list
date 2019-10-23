fc0bb9cb268079d4f4073e686209d9b03c7bc400
Yassin Bahassane <yassinfreethinker@gmail.com>
2019-10-23 20:55:53 +0100

    Prepare and deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -17,7 +17,17 @@ mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true})
         .catch( err => console.log(err));
 
 //Use routes
-app.use('/api/items',items);        
+app.use('/api/items',items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+        //Set static folder
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+}
 
 
 app.listen(port, console.log(`Server running on port ${port}`));
